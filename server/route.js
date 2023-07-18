006393374c37acfb6ed4e25124c9a9f996b7223b
artyomakulov <artyomakulov@gmail.com>
2023-07-18 15:51:13 +0300

    Update route.js

--- server/route.js
+++ server/route.js
@@ -1,8 +1,20 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", (req, res)=> {
-res.send("its my page")
-})
+router.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  res.send("its my page");
+});
 
 module.exports = router;
